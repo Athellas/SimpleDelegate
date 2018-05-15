6af3a5599d9104326ca3d0e57b24ba86e1849f66
Michal Krupinski <michal.krupinski@fuerogames.pl>
2018-05-15 17:17:41 +0200

    365

--- SimpleDelegate/Program.cs
+++ SimpleDelegate/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace SimpleDelegate
 {
-    class Program
+    public delegate int BinaryOp(int x, int y);
+
+    public class SimpleMath
+    {
+        public static int Add(int x, int y) { return x + y; }
+        public static int Substract(int x, int y) { return x - y; }
+        public static int SquareNumber(int a) { return a * a; }
+    }
+
+        class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Simple delgate example *****\n");
+
+            BinaryOp b = new BinaryOp(SimpleMath.Add);
+
+            Console.WriteLine("10 + 10 is {0}", b(10, 10));
+            Console.ReadLine();
         }
     }
 }
